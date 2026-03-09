690d51daae3f443de8a7ff34361bc4703cc9609c
Fyodor Kovalchuck <ppersidski@gmail.com>
2026-03-09 23:17:34 +0300

    feat: add bash script for testing

--- api_test_sh/test.sh
+++ api_test_sh/test.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
-curl -X POST "http://127.0.0.1:8000/perceptron/predict" \
-  -H "Content-Type: application/json" \
-  -d '{
-    "Age": 40,
-    "Sex": "M",
-    "ChestPainType": "ATA",
-    "RestingBP": 140,
-    "Cholesterol": 289,
-    "FastingBS": 0,
-    "RestingECG": "Normal",
-    "MaxHR": 172,
-    "ExerciseAngina": "N",
-    "Oldpeak": 0,
-    "ST_Slope": "Up"
-  }'
+
+CSV_FILE="../dataset/heart_test.csv"  # Укажите путь к вашему CSV файлу
+
+# Пропускаем заголовок и обрабатываем каждую строку
+tail -n +2 "$CSV_FILE" | while IFS=',' read -r Age Sex ChestPainType RestingBP Cholesterol FastingBS RestingECG MaxHR ExerciseAngina Oldpeak ST_Slope HeartDisease; do
+    
+    # Формируем JSON запрос
+    JSON_DATA=$(cat <<EOF
+{
+    "Age": $Age,
+    "Sex": "$Sex",
+    "ChestPainType": "$ChestPainType",
+    "RestingBP": $RestingBP,
+    "Cholesterol": $Cholesterol,
+    "FastingBS": $FastingBS,
+    "RestingECG": "$RestingECG",
+    "MaxHR": $MaxHR,
+    "ExerciseAngina": "$ExerciseAngina",
+    "Oldpeak": $Oldpeak,
+    "ST_Slope": "$ST_Slope"
+}
+EOF
+)
+    RESPONSE=$(curl -s -X POST "http://127.0.0.1:8000/perceptron/predict" \
+        -H "Content-Type: application/json" \
+        -d "$JSON_DATA")
+    
+    echo "$RESPONSE"
+    echo "---(real is heart disease: $HeartDisease)"
+    
+done
+
+echo "Обработка завершена"
